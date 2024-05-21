f1c090105c1eef6ae6b2c5c4302ed51f47afd142
bobkao <bobkaott@gmail.com>
2024-05-21 21:31:05 +0800

    新增

---
+++ spring-boot-user-registration-and-Login-main/src/main/resources/static/PS_icon.js
@@ -0,0 +1,16 @@
+var icons = [
+    {
+        icon: L.icon({
+            iconUrl: '../images/green.png',
+            iconSize: [15, 15],
+            iconAnchor: [8, 8]
+        })
+    },
+    {
+        icon: L.icon({
+            iconUrl: '../images/red.png',
+            iconSize: [15, 15],
+            iconAnchor: [8, 8]
+        })
+    }
+];
