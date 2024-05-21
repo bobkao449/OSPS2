f3e80b6fb6bc7260d347bf78b0069997b215207f
bobkao <bobkaott@gmail.com>
2024-05-22 06:08:11 +0800

    改名

--- OSPS2/src/main/java/net/enjoy/springboot/registrationlogin/Application.java
+++ OSPS2/src/main/java/net/enjoy/springboot/registrationlogin/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RegistrationLoginApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(RegistrationLoginApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
